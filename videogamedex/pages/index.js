68c45e227a7c1e5ee7e1a5b53fca267dec9c5e92
Jeanie Chea <chea.jeanie@gmail.com>
2022-09-18 00:33:47 -0700

    Update index.js

--- videogamedex/pages/index.js
+++ videogamedex/pages/index.js
@@ -1,6 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react'
 import GameCard from '../components/GameCard'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
