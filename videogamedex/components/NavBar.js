d7ea78f1b2eda18e23d74d4e9770355cbb3faef2
Jeanie Chea <chea.jeanie@gmail.com>
2022-09-18 01:03:26 -0700

    Nextjs being unreasonable

--- videogamedex/components/NavBar.js
+++ videogamedex/components/NavBar.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import styles from '../styles/NavBar.module.css'
+// import styles from '../styles/NavBar.module.css'
 
+// const NavBar = () => {
+//   return (
+//     <nav className={styles.nav}>
+//         <div className={styles.navtitle}>
+//             <h1>VideoGameDex</h1>
+//         </div>
+//         <div>
+//             <input placeholder='Search 790k+ games' type='text' className={styles.searchbar}/>
+//         </div>
+//     </nav>
+//   )
+// }
 const NavBar = () => {
   return (
-    <nav className={styles.nav}>
-        <div className={styles.navtitle}>
+    <nav >
+        <div >
             <h1>VideoGameDex</h1>
         </div>
         <div>
-            <input placeholder='Search 790k+ games' type='text' className={styles.searchbar}/>
+            <input placeholder='Search 790k+ games' type='text' />
         </div>
     </nav>
   )
