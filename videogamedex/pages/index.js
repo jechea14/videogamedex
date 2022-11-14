4d65db52a87585a53d3b6e3968127bfb6c009d5a
Jeanie Chea <chea.jeanie@gmail.com>
2022-11-14 10:55:44 -0800

    Replace infinite scrolling with "Load More" Button
    Infinite scrolling became too complicated to understand right now. Might properly learn observations later

--- videogamedex/pages/index.js
+++ videogamedex/pages/index.js
@@ -1,29 +1,45 @@
 import React, {useState, useRef, useCallback, useEffect} from 'react'
 import GameCard from '../components/GameCard'
 import styles from '../styles/Home.module.css'
-import axios from 'axios'
-
 import NavBar from '../components/NavBar'
 
 export default function Home({games}) {
   console.log(games)
-
+  const [data, setData] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState("")
-  const [data, setData] = useState([])
-  const [page, setPage] = useState(1)
-  const [loading, setLoading] = useState(false)
-  const [hasMore, setHasMore] = useState(false)
-  const observer = useRef()
 
-  const [mobileNavShown, setMobileNavShown] = useState(false)
-  
+  useEffect(() => {
+    setLoading(true);
+    const getNextGames = async () => {
+      console.log(nextPage);
+      const getGames = await fetch(
+        `https://api.rawg.io/api/games?key=56900b065e5d4c2d923515e904b9edb6&page=${nextPage}&search=${query}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          setData([...data, ...json['results']]);
+        });
+      setLoading(false);
+    };
+    console.log(data);
+    getNextGames();
+  }, [nextPage]);
+
+  useEffect(() => {
+    setData([])
+  }, [query])
+
   const useDebounce = (value, delay) => {
     const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
       const handler = setTimeout(() => {
         setDebouncedValue(value)
-        setPage(1)
+        setNextPage(1)
       }, delay)
 
       return () => {
@@ -36,88 +52,41 @@ export default function Home({games}) {
 
   const debouncedSearch = useDebounce(query, 500)
 
-  const lastElement = (node) => {
-    if (loading) return;
-    if (observer.current) observer.current.disconnect();
-
-    observer.current = new IntersectionObserver((entries) => {
-      console.log("in", entries);
-      if (entries[0].isIntersecting && hasMore) {
-        setPage((prevPageNumber) => prevPageNumber + 1);
-      }
-    })
-    console.log("node", node);
-    if (node) observer.current.observe(node)
-  }
-
-  console.log("observer", observer)
-
-  useEffect(() => {
-    setData([])
-  }, [query])
-
-  useEffect(() => {
-    setLoading(true)
-    const getSearchItems = async () => {
-      const games = await axios.get(`https://api.rawg.io/api/games?key=56900b065e5d4c2d923515e904b9edb6&page=${page}&search=${query}`)
-      console.log(games)
-      setLoading(false)
-      setHasMore(games.data.results.length > 0)
-      setData((prev) => {
-        return [...new Set([...prev, ...games.data.results.map((game) => <GameCard 
-          name={game.name} 
-          img={game.background_image} 
-          slug={game.slug} 
-          id={game.id} 
-          metascore={game.metacritic}
-          platform={game.parent_platforms}
-          key={game.id} 
-          />)])]
-      })
-      console.log(data)
-    }
-
-    if (debouncedSearch) getSearchItems();
-    
-  }, [debouncedSearch, page])
+  const handleNextPage = () => {
+    setNextPage(nextPage + 1);
+  };
 
   const handleChange = (e) => {
     setQuery(e.target.value)
   }
 
-  const shouldStartSearch = query.length > 0
+  useEffect(() => {
+    setLoading(true)
+    const getSearchItems = async () => {
+      const getGames = await fetch(
+        `https://api.rawg.io/api/games?key=56900b065e5d4c2d923515e904b9edb6&page=${nextPage}&search=${query}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          setData([...data, ...json['results']]);
+        });
+      setLoading(false)
+      console.log(data)
+    }
+
+    if (debouncedSearch) getSearchItems();
+    
+  }, [debouncedSearch, nextPage])
 
   return (
     <div>
       <NavBar/>
       <input placeholder='Search 790k+ games' type="search" className={styles.searchbar} onChange={(e) => handleChange(e)}/>
       <div className={styles.gameContainer}>
-
-      {
-        shouldStartSearch ? data.map((game, index) => {
-          
-          return <div key={index} ref={lastElement}>{game}</div>
-        }) : <div>      <h1>All Games</h1>
-        
-          {
-            games.results.map((game, index) => {
-              if(games.length === index + 1) {
-                return (
-                  <div ref={lastElement} key={game.id}>
-                    <GameCard 
-                    name={game.name} 
-                    img={game.background_image} 
-                    slug={game.slug} 
-                    id={game.id} 
-                    metascore={game.metacritic}
-                    platform={game.parent_platforms}
-                    key={game.id} 
-                    />
-                  </div>
-                )
-              }
-              else {
-                return (
+        {data.map((game) => {
+          return (
                   <GameCard 
                   name={game.name} 
                   img={game.background_image} 
@@ -127,27 +96,16 @@ export default function Home({games}) {
                   platform={game.parent_platforms}
                   key={game.id} 
                   />
-                )
-              }
-            })
-          }
-        </div>
+              )
+        })}
 
-      }
       </div>
-          {/* <div className={debouncedSearch}>
-            {
-              shouldStartSearch &&
-
-              data.map((game, index) => {
-                return <div key={index} ref={lastElement}>{game}</div>
-              })
-            }
-          </div> */}
-
+      {
+        !loading && <button onClick={handleNextPage}>Load More</button>
+      }
       <div>{loading && 'Loading...'}</div>
     </div>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
