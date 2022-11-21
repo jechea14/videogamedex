ce3e1b4f355245f0cf5564421d2fcefdcbc9d5db
Jeanie Chea <chea.jeanie@gmail.com>
2022-11-20 23:05:07 -0800

    CSS - Finish styling

--- videogamedex/components/InfoCard.jsx
+++ videogamedex/components/InfoCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const InfoCard = ({title, desc}) => {
   return (
-    <div>
+    <>
         <h2>{title}</h2>
         <p>{desc}</p>
-    </div>
+    </>
   )
 }
 
--- videogamedex/pages/games/[id]/index.js
+++ videogamedex/pages/games/[id]/index.js
@@ -8,79 +8,87 @@ const games = ({game}) => {
     console.log(game)
 
     return (
-      <>
-        <NavBar/>
         <div className={styles.container}>
+          <NavBar/>
           <h1>{game.name}</h1>
 
-          <Image 
-            src={game.background_image} 
-            alt={game.name} 
-            width={4000} 
-            height={2500} 
-            loading="lazy"
-          />
-
-          {
-            game.background_image_additional === null ? 
-            <></> : <Image 
-                    src={game.background_image_additional} 
-                    alt={game.name} 
-                    width={4000} 
-                    height={2500} 
-                    loading="lazy"
-                    />
-          }
-          <div className={styles.infoCard}>
-            <InfoCard title={"About"} desc={game.description_raw}/>
-          </div>
-          <div className={styles.infoCard}>
-            <h2>Genre</h2>
+          <div className={styles.imgContainer}>
+            <Image 
+              src={game.background_image} 
+              alt={game.name} 
+              width={4000} 
+              height={2500} 
+              loading="lazy"
+            />
             {
-              game.genres.map((genre) => {
-                return <p key={genre.name}>{genre.name}</p>
-              })
+              game.background_image_additional === null ? 
+              <></> : <Image 
+                      src={game.background_image_additional} 
+                      alt={game.name} 
+                      width={4000} 
+                      height={2500} 
+                      loading="lazy"
+                      />
             }
-          </div>
-          <div className={styles.infoCard}>
-            <h2>Platforms</h2>
-            {
-              game.platforms.map((platform) => {
-                return <p key={platform.platform.name}>{platform.platform.name}</p>
-              })
-            }
-          </div>
-          <div className={styles.infoCard}>
-            <InfoCard title={"Release Date"} desc={game.released}/>
-          </div>
-          <div className={styles.infoCard}>
-            {/* <InfoCard title={"Metascore"} desc={game.metacritic}/> */}
-            <h2>Metascore</h2>
-            <Metascore metascore={game.metacritic}/>
-          </div>
-          <div className={styles.infoCard}>
-            <h2>Publisher</h2>
-            {
-              game.publishers.map((publisher) => {
-                return <p key={publisher.name}>{publisher.name}</p>
-              })
-            }
-          </div>
-          <div className={styles.infoCard}>
-            <h2>Developer</h2>
-            {
-              game.developers.map((developer) => {
-                return <p key={developer.name}>{developer.name}</p>
-              })
-            }
-          </div>
-          <div className={styles.infoCard}>
-            <h2>Website</h2>
-            <a href={game.website} target="_blank" rel="noreferrer">{game.website}</a>
           </div>
 
+          <div className={styles.bodyContainer}>
+            <div className={styles.infoCard}>
+              <InfoCard title={"About"} desc={game.description_raw}/>
+            </div>
+
+            <div className={styles.detailContainer}>
+              <div className={styles.infoCard}>
+                <h2>Metascore</h2>
+                <Metascore metascore={game.metacritic}/>
+              </div>
+              <div className={styles.infoCard}>
+                <h2>Genre</h2>
+                {
+                  game.genres.map((genre) => {
+                    return <p key={genre.name}>{genre.name}</p>
+                  })
+                }
+              </div>
+              <div className={styles.infoCard}>
+                <h2>Platforms</h2>
+                {
+                  game.platforms.map((platform) => {
+                    return <p key={platform.platform.name}>{platform.platform.name}</p>
+                  })
+                }
+              </div>
+              <div className={styles.infoCard}>
+                <InfoCard title={"Release Date"} desc={game.released}/>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className={styles.creaters}>
+            <div className={styles.infoCard}>
+              <h2>Publisher</h2>
+              {
+                game.publishers.map((publisher) => {
+                  return <p key={publisher.name}>{publisher.name}</p>
+                })
+              }
+            </div>
+            <div className={styles.infoCard}>
+              <h2>Developer</h2>
+              {
+                game.developers.map((developer) => {
+                  return <p key={developer.name}>{developer.name}</p>
+                })
+              }
+            </div>
+            <div className={styles.infoCard}>
+              <h2>Website</h2>
+              <a href={game.website} target="_blank" rel="noreferrer">{game.website}</a>
+            </div>
+          </div>
         </div>
-      </>
       )
 }
 
